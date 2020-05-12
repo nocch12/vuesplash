842f7df412bd554a4c355a4028d4859eafe49a14
Kohki Ohno <k.ohno12.gt@gmail.com>
2020-05-12 21:33:47 +0900

    ch8まで

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -18,6 +18,14 @@
     </ul>
     <div class="panel" v-show="tab === 1">
       <form class="form" @submit.prevent="login">
+        <div v-if="loginErrors" class="errors">
+          <ul v-if="loginErrors.email">
+            <li v-for="msg in loginErrors.email" :key="msg">{{ msg }}</li>
+          </ul>
+          <ul v-if="loginErrors.password">
+            <li v-for="msg in loginErrors.password" :key="msg">{{ msg }}</li>
+          </ul>
+        </div>
         <label for="login-email">Email</label>
         <input
           type="text"
@@ -39,6 +47,17 @@
     </div>
     <div class="panel" v-show="tab === 2">
       <form class="form" @submit.prevent="register">
+        <div v-if="registerErrors" class="errors">
+          <ul v-if="registerErrors.name">
+            <li v-for="msg in registerErrors.name" :key="msg">{{ msg }}</li>
+          </ul>
+          <ul v-if="registerErrors.email">
+            <li v-for="msg in registerErrors.email" :key="msg">{{ msg }}</li>
+          </ul>
+          <ul v-if="registerErrors.password">
+            <li v-for="msg in registerErrors.password" :key="msg">{{ msg }}</li>
+          </ul>
+        </div>
         <label for="username">Name</label>
         <input
           type="text"
@@ -76,6 +95,8 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   data() {
     return {
@@ -93,17 +114,21 @@ export default {
     };
   },
   computed: {
-    apiStatus() {
-      return this.$store.state.auth.apiStatus;
-    }
+    ...mapState({
+      apiStatus: state => state.auth.apiStatus,
+      loginErrors: state => state.auth.loginErrorMessages,
+      registerErrors: state => state.auth.registerErrorMessages
+    })
   },
   methods: {
     async register() {
       // authストアのresigterアクションを呼び出す
       await this.$store.dispatch("auth/register", this.registerForm);
 
-      // トップページに移動する
-      this.$router.push("/");
+      if (this.apiStatus) {
+        // トップページに移動する
+        this.$router.push("/");
+      }
     },
     async login() {
       // authストアのloginアクションを呼び出す
@@ -113,7 +138,13 @@ export default {
         // トップページに移動する
         this.$router.push("/");
       }
+    },
+    clearError() {
+      this.$store.commit("auth/setLoginErrorMessages", null);
     }
+  },
+  created() {
+    this.clearError();
   }
 };
 </script>
