4cd4d852d67720ae74c6ba768ead51c152b0f10d
Kohki Ohno <k.ohno12.gt@gmail.com>
2020-05-15 23:28:27 +0900

    写真投稿バックエンド

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,6 @@ Route::post('/register', 'Auth\RegisterController@register')->name('register');
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 // ログアウト
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+// 写真投稿
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
